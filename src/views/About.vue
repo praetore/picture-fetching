da753888ecfc4d77bb113a2160a0b7199f9891c8
wouternaloop <wouternaloop@penguin>
2019-07-13 03:09:20 +0200

    for loop example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,17 @@
 <template>
   <q-page padding>
     <p>This is an about page</p>
+    <ul id="data">
+      <li :key="item" v-for="item in numbers">{{ item }}</li>
+    </ul>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageAbout'
+  name: 'PageAbout',
+  data: () => ({
+    numbers: [1,2,3]
+  })
 }
 </script>
